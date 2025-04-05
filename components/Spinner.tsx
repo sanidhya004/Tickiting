808740a0d65b00bf2df63b8f8ec62522c81f66f9
Sanidhya Sharma <80572505+sanidhya004@users.noreply.github.com>
2025-04-05 13:25:36 +0530

    Update Spinner.tsx

--- components/Spinner.tsx
+++ components/Spinner.tsx
@@ -1,7 +1,7 @@
 function Spinner() {
     return (
       <div className="flex justify-center items-center min-h-[200px]">
-        <div className="w-8 h-8 border-4 border-blue-600 border-t-transparent rounded-full animate-spin" />
+        <div className="w-8 h-8 border-4 border-blue-500 border-t-transparent rounded-full animate-spin" />
       </div>
     );
   }
